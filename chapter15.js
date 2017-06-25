dfab76719a3d2e73717244a31f3c3d4b8ac2bfe8
Judy <850832627@qq.com>
2017-06-25 08:35:21 +0800

    Update chapter15.js

--- chapter15.js
+++ chapter15.js
@@ -20,3 +20,53 @@ var fistpara = document.getElementsByTagName("p")[0];
 var snapshot = Array.prototype.slice.call(nodelist,0);
 
 
+/*15.3
+parentNode
+childNodes
+firstChild lastChild
+nextSibling previousSibling
+nodeType
+nodeValue
+nodeName
+*/
+function parent(e,n){
+  if(n===undefined){n=1;}
+  while(n--&&e){n=1;}
+  if(!e||e.nodeType!==1){return null;}
+  return e;
+}
+
+function sibling(e,n){
+  while(e&&n!==0){
+    if(n>0){
+      if(e.nextElementSibling){
+        e=e.nextElementSibling;
+      }else{
+        for(e=e.nextElementSibling;e&&e.nodeType!==1;e=e.nextSibling)
+             /*空循环*/
+      }
+      n--;
+    }else{
+      
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
