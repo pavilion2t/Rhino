5a83e5fe2c6058b14a6e51041b4455822772dd92
pavilion <850832627@qq.com>
2017-09-26 10:39:44 +0800

    7.8

---
+++ chapter7/array.js
@@ -0,0 +1,20 @@
+//7.8 
+//join()
+var a = [1,2,3];
+a.join();//"1,2,3"
+a.join(" ");//"1 2 3"
+a.join("");//"123"
+var b = new Array(10);
+b.join("-");//"---------"
+
+//reverse()
+var a =[1,2,3];
+a.reverse().join();
+
+//sort()
+//concat()
+//slice()
+//splice()
+//push() pop()
+//unshift() shift()
+//toString() toLocalString()
