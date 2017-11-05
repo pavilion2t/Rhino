6bb44332d99c8fbdae58bb69cabd27a0b718d351
Judy <850832627@qq.com>
2017-11-05 06:28:22 -0600

    Update 8_5.js

--- chapter8/8_5.js
+++ chapter8/8_5.js
@@ -1,5 +1,6 @@
 //定义一个扩展函数，用来将第二个以及后续参数复制到第一个参数
 var extend = (function(){
+ 
   for(var p in {toString: null}) {
     return function extend(o) {
       for(var i = 1; i < arguments.length; i++){
@@ -9,3 +10,19 @@ var extend = (function(){
       return o;
     };
   }
+  
+  return function patched_extend(o){
+    foe(var i = 1; i < arguments.length; i++){
+      var source = arguments[i];
+      for(var prop in source) o[prop] = source[prop];
+      for(var j = 0; j < protoprops.length; j++){
+        prop = protoprops[j];
+        if(source.hasOwnProperty(prop))  o[prop] = source[prop];
+      }
+   }
+   return o;
+};
+  
+  var protoprops = ["toString", "valueOf", "constructor", "hasOwnProperty",
+                    "isPrototypeof", "propertyIsEnumerable", "toLocalString"];
+}());
