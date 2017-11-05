83cae5e681df13c02de5e4974368f85f1197aa64
Judy <850832627@qq.com>
2017-11-05 07:51:22 -0600

    Update 8_8.js

--- chapter8/8_8.js
+++ chapter8/8_8.js
@@ -11,5 +11,43 @@ for(var i = 0; i < data.length; i++){
 }
 var stddev = Math.sqrt(total/(data.length-1));
 
+//可以使用数组方法map() reduce()来实现同样的计算
+var sum = function(x,y) { return x+y; }
+var square = function(x) { return x*x; }
 
+var data = [1,1,3,5,5,];
+var mean = data.reduce(sum)/data.length;
+var deviations = data.map( function(x) { return x-mean; });
+var stddev = Math.sqrt(deviations.map(square).reduce(sum)/(data.length-1));
+
+
+//基于ES3实现map() reduce()
+var map = Array.prototype.reduce 
+    ?function(a,f,initial){
+      if(arguments.lenght > 2) 
+       return a.reduce(f,initial);
+       else return a.reduce(f);
+    }
+    :function(a,f,initial){
+      var i = 0, len = a.length,accumulator;
+      if(arguments.length > 2) accumulator = initial;
+      else { 
+        if (len == 0) throw TypeError();
+        while(i < len){
+          if(i in a){
+            accumulator = a[i++];
+            break;
+          }
+          else i++;
+        }
+        if{ i == len) throw TypeError();
+   }
+    
+  while (i<len){
+    if(i in a)
+      accumulator = f.call(undefined,accumulator,a[i],i,a);
+      i++;
+  }
+    return accumulator;
+ }
 
