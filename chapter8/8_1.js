d45d519bdb734a2596f5ce4580c933741e78fb1e
Judy <850832627@qq.com>
2017-11-05 01:28:33 -0500

    Update 8_1.js

--- chapter8/8_1.js
+++ chapter8/8_1.js
@@ -1,4 +1,4 @@
-//8.1函数语句和表达式  2种方式定义函数
+//8.1函数语句和表达式  🔥 2种方式定义函数 
 
 //输出o的每个属性的值，返回undefined
 function printprops(o){
@@ -31,15 +31,32 @@ function factorial(x){
   return x * factorial(x-1);
 }
 
-{/* 函数表达式，注意把它赋值给了一个变量 */}
+{/* 
+🔥🔥🔥 
+函数表达式，注意把它赋值给了一个变量
+*/}
 var square = function(x){
   return x * x;
 }
 
 
+{/* 
+函数表达式可以包含名称
+这在递归时很有用
+*/}
+var f = function fact(x){
+  if( x <= 1 ) return 1;
+  return x * fact(x-1);
+}
+  
+//函数表达式也可以作为参数传给其他函数
+data.sort(function(a,b){ return a-b; }};
+          
+//函数表达式有时定义后立即调用
+var tensquared = (function(x){ return x*X;}(10));
 
-{/*  */}
-
-{/*  */}
-
-{/*  */}
+{/*
+声明语句可以在定义之前调用。
+以表达式定义的函数，无法在定义之前调用。
+因为变量声明可以提前，但是变量赋值不会提前。
+*/}
