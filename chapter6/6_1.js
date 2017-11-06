3e7b4d42646ec4341b7c17a9551f2062167db4a8
Judy <850832627@qq.com>
2017-11-06 09:56:04 -0600

    Update 6_1.js

--- chapter6/6_1.js
+++ chapter6/6_1.js
@@ -1,1 +1,15 @@
 //通过原型链继承创建一个新对象
+function inherit(){
+  if(p == null) throw TypeError();
+  if(Object.create)
+    return Object.create(p);
+  var t = typeof p;
+  if(t !== "Object" && t !== "function") throw TypeError();
+  function f(){ };
+  f.prototype = p;
+  return new f();
+}
+
+var o = { x: "don't change this value" };
+library_function(inherit(o));
+  
