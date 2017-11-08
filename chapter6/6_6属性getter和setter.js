a83e5b1582291c2bbefbeb438dd1b085f924d29f
Judy <850832627@qq.com>
2017-11-08 08:36:33 -0600

    Update 6_6属性getter和setter.js

--- chapter6/6_6属性getter和setter.js
+++ chapter6/6_6属性getter和setter.js
@@ -11,3 +11,19 @@ var p = {
  },
  get theta(){ return Math.atan2(this.y, this.x); }
 }
+
+
+var serialnum = {
+  $n: 0,
+  get next(){ return this.$n++; }
+  set next(n){
+    if(n >= this.$n) this.$n = n;
+    else throw "can not be smaller than current value";
+  }
+};
+
+var random = {
+  get octet(){ return Math.floor(Math.random()*256); },
+  get unit16(){ return Math.floor(Math.random()*65536); },
+  get init16(){ return Math.floor(Math.random()*65536)-32768; }
+}
