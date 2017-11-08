1fcb8f35616c5ccfdc2af925aeeca05e4c9d027d
Judy <850832627@qq.com>
2017-11-08 08:57:14 -0600

    Update 6_7属性的特性.js

--- chapter6/6_7属性的特性.js
+++ chapter6/6_7属性的特性.js
@@ -2,5 +2,12 @@ Object.defineProperty(Object.prototype,"extends",{
   writable:true,
   enumerable:false,
   configurable:true,
-  value:function(o){}
+  value:function(o){
+    var names = Object.getOwnPropertyNames(o);
+    for(var i = 0; i < names.length; i++){
+      if(name[i] in this) continue;
+      var desc = Object.getOwnPropertyDescriptor(o,names[i]);
+      Object.defineProperty(this,names[i],desc);
+    }
+   }
   });
