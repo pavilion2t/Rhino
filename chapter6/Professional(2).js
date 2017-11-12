f84ebb62808e85ef5381f3b2550d678bba604e58
Judy <850832627@qq.com>
2017-11-11 21:26:28 -0600

    Update Professional(2).js

--- chapter6/Professional(2).js
+++ chapter6/Professional(2).js
@@ -107,3 +107,23 @@ Person.prtototype = {
   job: "journalism",
   sayName :function(){ alert(this.name); }
 }
+
+//组合使用构造函数模式和原型模式
+function Person(name,age,job){
+  this.name = name;
+  this.age = age;
+  this.job = job;
+  this.friends = ["Shelby", "Court"];
+}
+Person.prototype = {
+  constructor: Person,
+  sayName: function(){ alert(this.name); }
+}
+var person1 = new Person("Nicholas",29,"Software Engineer");
+var person2 = new Person("Greg",27,"Doctor");
+
+person1.friends.push("Van");
+alert(person1.friends);
+alert(person2.friends);
+alert(person1.friends === person2.friends); //false
+alert(person1.sayName === person2.sayName); //true
